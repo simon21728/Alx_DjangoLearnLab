59d1b887ab8db4b351fa44d7d6e2ade420c283c3
sewmehon bayu <simonbayu2211@gmail.com>
2025-02-13 22:30:32 -0900

    Implementing and Interacting with Django Models

--- Introduction_to_Django/LibraryProject/bookshelf/create.md
+++ Introduction_to_Django/LibraryProject/bookshelf/create.md
@@ -1,1 +1,13 @@
-This is some text
+# Creating Books in LibraryProject
+
+This document explains how to create new book records in the `LibraryProject` using the `Book` model.
+
+## Creating a Book via Django Shell
+
+To create a new book record, you can use the Django shell to interact with your models directly.
+
+### Steps:
+
+1. **Open the Django shell**:
+   ```bash
+   python manage.py shell
--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+from bookshelf.models import Book
+
+# Create a new Book instance
+new_book = Book.objects.create(
+    title="1984",  # Book Title
+    author="George Orwell",  # Book Author
+    published_date="1949-06-08",  # Date when the book was published
+    isbn="9780451524935",  # ISBN number for the book
+    pages=328,  # Number of pages
+    language="English"  # Language of the book
+)
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
@@ -7,3 +19,6 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+books = Book.objects.all()
+print(books)
