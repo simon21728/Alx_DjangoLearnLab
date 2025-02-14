ce38afb71eeb8afe9b23c373f54186652cad1d7d
sewmehon bayu <simonbayu2211@gmail.com>
2025-02-14 01:41:17 -0900

    Updated list_filter to filter books by publication year

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
 # bookshelf/admin.py
 from django.contrib import admin
 from .models import Book
+
 class BookAdmin(admin.ModelAdmin):
     # Specify which fields to display in the list view
     list_display = ('title', 'author', 'published_date')
-
-    # Add search functionality for title and author fields
+    
+    # Enable search functionality for title and author fields
     search_fields = ('title', 'author')
 
-    # Add filter options based on publication year
+    # Configure the filter options: Filter by published date (year)
     list_filter = ('published_date',)
 
-# Register the Book model with the default admin interface
-admin.site.register(Book,BookAdmin)
+admin.site.register(Book, BookAdmin)
--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -19,13 +19,20 @@ class BookAdmin(admin.ModelAdmin):
 admin.site.register(Book, BookAdmin)
 
 
+# bookshelf/models.py
+from django.db import models
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
-
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()  # This is likely the field you are referring to
+    isbn = models.CharField(max_length=13)
+    pages = models.IntegerField()
+    language = models.CharField(max_length=50)
+    
     def __str__(self):
         return self.title
+
 
 
 book = Book.objects.create(title="1984", author="George Orwell", publication_year=1949)
