ed042497f4f11799284c0231479109d62988ec04
sewmehon bayu <simonbayu2211@gmail.com>
2025-02-14 01:44:13 -0900

    Updated list_filter to filter books by publication year

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 
-# bookshelf/admin.py
 from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    # Specify which fields to display in the list view
     list_display = ('title', 'author', 'published_date')
-    
-    # Enable search functionality for title and author fields
     search_fields = ('title', 'author')
-
-    # Configure the filter options: Filter by published date (year)
+    
+    # Filter by year
     list_filter = ('published_date',)
+    
+    def get_published_year(self, obj):
+        return obj.published_date.year
+    get_published_year.admin_order_field = 'published_date'  # Allows sorting by year
+    get_published_year.short_description = 'Publication Year'
 
 admin.site.register(Book, BookAdmin)
+
