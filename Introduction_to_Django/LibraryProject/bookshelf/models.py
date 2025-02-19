c4ad871f2ee08a709ee1ce1ce983085779bb2560
sewmehon bayu <simonbayu2211@gmail.com>
2025-02-19 04:29:03 -0900

    Implementing Advanced Model Relationships in Django

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,34 +1,21 @@
-from django.db import models
-from .models import Book
-
 # bookshelf/models.py
+
 from django.db import models
 
+# Author model
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# Book model (Notice how we're using a string reference here)
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    published_date = models.DateField()  # This is likely the field you are referring to
-    isbn = models.CharField(max_length=13)
-    pages = models.IntegerField()
-    language = models.CharField(max_length=50)
-    
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_date = models.DateField()  # Make sure this field is defined here
+
     def __str__(self):
         return self.title
 
-
-
-book = Book.objects.create(title="1984", author="George Orwell", publication_year=1949)
-
-
-book = Book.objects.get(title="1984")
-print(f"Title: {book.title}, Author: {book.author}, Year: {book.publication_year}")
-
-
-book = Book.objects.get(title="1984")
-book.title = "Nineteen Eighty-Four"
-book.save()
-
-
-book = Book.objects.get(title="Nineteen Eighty-Four")
-book.delete()
-Book.objects.all()
