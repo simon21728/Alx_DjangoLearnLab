5f8534164a6b9970bff387aa3cce9f3689a51c01
sewmehon bayu <simonbayu2211@gmail.com>
2025-02-19 21:32:00 -0900

    Implementing Advanced Model Relationships in Django

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,11 @@
+# bookshelf/admin.py
 from django.contrib import admin
-from .models import Book
+from .models import Book  # Import the Book model
 
-@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Fields to display in the list view
-    list_filter = ('publication_year',)  # Add publication_year to the list filters
-    search_fields = ('title', 'author')  # Optional: Add search functionality
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
+
+# Register the Book model along with the BookAdmin configuration
+admin.site.register(Book, BookAdmin)
--- Introduction_to_Django/LibraryProject/bookshelf/views.py
+++ Introduction_to_Django/LibraryProject/bookshelf/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def some_function():
+    from bookshelf.models import Book  # type: ignore # Import inside the function
+    books = Book.objects.all()
+    # Rest of the code
