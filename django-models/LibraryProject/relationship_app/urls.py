19580903696dcda318670e8207a3a44981e21518
sewmehon bayu <simonbayu2211@gmail.com>
2025-02-20 03:33:57 -0900

    Django Views and URL Configuration

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,13 @@
+# relationship_app/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # URL pattern for function-based view
+    path('books/', views.list_books, name='list_books'),
+    
+    # URL pattern for class-based view
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book
+from .models import Library
+from django.views.generic import DetailView
+# Function-based view to list all books and their authors
+def list_books(request):
+    books = Book.objects.all()  # Get all books from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    # Override the get_context_data method to include all books in the library
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()  # Get books related to the library
+        return context
