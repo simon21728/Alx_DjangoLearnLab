51c78f23f3be5e8e37527d3f319f26a074a27a15
sewmehon bayu <simonbayu2211@gmail.com>
2025-02-21 02:42:39 -0900

    Implementing Custom Permissions in Django

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -15,6 +15,12 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     publication_date = models.DateField()  # Make sure this field is defined here
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
     def __str__(self):
         return self.title
