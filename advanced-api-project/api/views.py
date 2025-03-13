972b422a07783d06badc8022fbf6613352f8374d
sewmehon bayu <simonbayu2211@gmail.com>
2025-03-13 01:13:36 -0900

    Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly  # Import permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # List all books
 class BookListView(generics.ListAPIView):
